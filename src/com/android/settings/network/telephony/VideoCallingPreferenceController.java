f245e7c27be48bc8d6f1645b376c19e4ce0fc6a0
Josh Hou <joshhou@google.com>
2019-10-22 16:16:35 +0800

    Fix ViLTE is not available when WiFi Calling is disabled
    Bug: 141831293
    Test: 1. Set carrier config carrier_wfc_ims_available_bool to false,
    then verify Wi-Fi calling option is not shown.
          2. Set carrier config carrier_vt_available_bool to false, then
          verify video calling option is not shown.
          3. Set both carrier_wfc_ims_available_bool and
          carrier_vt_available_bool to false, then verify calling category
          is not shown.
    
    Change-Id: Ibec13fbeb5acf1d62342286687b2ba969beb4e14
    Merged-In: Ibec13fbeb5acf1d62342286687b2ba969beb4e14
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/network/telephony/VideoCallingPreferenceController.java
+++ src/com/android/settings/network/telephony/VideoCallingPreferenceController.java
@@ -64,7 +64,6 @@ public class VideoCallingPreferenceController extends TelephonyTogglePreferenceC
     @Override
     public int getAvailabilityStatus(int subId) {
         return subId != SubscriptionManager.INVALID_SUBSCRIPTION_ID
-                && MobileNetworkUtils.isWifiCallingEnabled(mContext, subId)
                 && isVideoCallEnabled(subId)
                 ? AVAILABLE
                 : CONDITIONALLY_UNAVAILABLE;
